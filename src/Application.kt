5974b1b816ddd969ec61eade901229f0bce2082e
linh.do <linh.do@seldatinc.com>
2019-05-22 15:17:55 +0700

    Init project

---
+++ src/Application.kt
@@ -0,0 +1,67 @@
+package linhdo.backend.inface
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.sessions.*
+import io.ktor.features.*
+import io.ktor.gson.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(DataConversion)
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+
+        get("/session/increment") {
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+data class MySession(val count: Int = 0)
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
